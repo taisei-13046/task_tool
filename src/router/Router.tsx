a01fab97c70a650d7d17d77edaed87453f5eb6d2
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2021-10-09 16:22:32 +0900

    routeの設定と初期設定

---
+++ src/router/Router.tsx
@@ -0,0 +1,18 @@
+import { memo, VFC } from "react";
+import { Route, Switch } from "react-router-dom";
+
+import {Home} from "../components/pages/Home"
+import {Task} from "../components/pages/Task"
+
+export const Router: VFC = memo(() => {
+  return (
+    <Switch>
+        <Route exact path="/">
+			<Home />
+        </Route>
+		<Route path="/task">
+			<Task />
+		</Route>
+	</Switch>
+	)
+});
