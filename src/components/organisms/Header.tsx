c3f3f4d24b7a18355dfda67bd019896a8a511054
半田隆太 <handaryuuta@handaryuutanoMacBook-Air.local>
2021-11-02 16:08:16 +0900

    メニューボタンを追加

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -6,6 +6,8 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import { useHistory } from 'react-router';
+import { MenuDrawer } from '../module/MenuDrawer';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,24 +18,38 @@ const useStyles = makeStyles((theme) => ({
   },
   title: {
     flexGrow: 1,
+    cursor: "pointer",
   },
 }));
 
 export const Header = () => {
   const classes = useStyles();
+  const history = useHistory();
+  const [ drawerOpen, setDrawerOpen ] = useState(false);
+	const onCLickHome = useCallback(() => history.push("/"), [history]);
+	const onCLickTask = useCallback(() => history.push("/task"), [history]);
+  const handleDrawerToggle = () => {
+    setDrawerOpen(!drawerOpen);
+  }
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <IconButton edge="start" onClick={handleDrawerToggle} className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" className={classes.title}>
+          <Typography variant="h6" className={classes.title} onClick={onCLickHome}>
            Task Tool
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
+      <MenuDrawer 
+        drawerOpen={drawerOpen}
+        handleDrawerToggle={handleDrawerToggle}
+        onCLickHome={onCLickHome}
+        onCLickTask={onCLickTask}
+      />
     </div>
   );
 }
